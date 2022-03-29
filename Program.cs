1b1bd12b4ac5402706610c423593b144f8073499
ppetrescu-wasdi <petru.petrescu@wasdi.cloud>
2022-03-29 12:16:01 +0200

    adding WASDI nature to the application; make a HelloWasdi call

--- Program.cs
+++ Program.cs
@@ -1,10 +1,22 @@
-﻿namespace TutorialSeeSharpApp
+﻿using WasdiLib;
+
+namespace TutorialSeeSharpApp
 {
-    internal class Program
+    internal class Program : IWasdiRunnable
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Wasdi wasdi = new();
+            wasdi.Init();
+            wasdi.SetVerbose(true);
+
+            Program program = new Program();
+            program.Run(wasdi);
+        }
+
+        public void Run(Wasdi wasdi)
+        {
+            wasdi.WasdiLog(wasdi.Hello());
         }
     }
 }
